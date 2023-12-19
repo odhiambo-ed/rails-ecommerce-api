55e065a97ba6b3a6b275a5ede68a8e76d6a64e89
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-19 13:44:52 +0300

    validates products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :company_id, presence: true
+  validates :category_id, presence: true
+
   belongs_to :company
   belongs_to :category
 end
