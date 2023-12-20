bc91a66fba13b5550b7942d6b2205177787c2848
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 16:35:36 +0300

    validate order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  # validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+
   belongs_to :product
   belongs_to :user
 end
