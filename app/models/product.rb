3b64382729e10721d6e9cd29830cf90c12549a5f
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 17:57:21 +0300

    add order relationship to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   validates :company_id, presence: true
   validates :category_id, presence: true
 
+  has_many :orders, dependent: :destroy
+
   belongs_to :company
   belongs_to :category
 end
