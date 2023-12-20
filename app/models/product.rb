e0361a8bbc833eaec42af6e3413fe527e97d83d1
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-20 16:13:21 +0300

    add format validation to price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }, format: { with: /\A\d+(\.\d{1,2})?\z/, message: "should be a valid decimal number with up to two decimal places" }
   validates :company_id, presence: true
   validates :category_id, presence: true
 
